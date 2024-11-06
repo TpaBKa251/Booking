65ab5452ad7c041dbb3897b4a30cebd421d605a1
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-11-06 18:39:23 +0700

    Исправления

---
+++ src/main/java/ru/tpu/hostel/booking/client/UserServiceClient.java
@@ -0,0 +1,17 @@
+package ru.tpu.hostel.booking.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@Component
+@FeignClient(name = "user-userservice", url = "http://userservice:8080")
+public interface UserServiceClient {
+
+    @GetMapping("/users/get_by_id/{id}")
+    ResponseEntity<?> getUserById(@PathVariable UUID id);
+}
--- src/main/resources/db/migration/V1__booking_tables.sql
+++ src/main/resources/db/migration/V1__booking_tables.sql
@@ -1,22 +1,22 @@
 CREATE TABLE "booking"
 (
-    id           UUID NOT NULL,
+    id           UUID                        NOT NULL UNIQUE,
     start_time   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     end_time     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     status       VARCHAR(255),
     type         VARCHAR(255),
-    "user"         UUID,
+    "user"       UUID,
     time_slot_id UUID,
     CONSTRAINT pk_booking PRIMARY KEY (id)
 );
 
 CREATE TABLE "time_slots"
 (
-    id         UUID         NOT NULL,
+    id         UUID                        NOT NULL UNIQUE,
     start_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     end_time   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    type       VARCHAR(255) NOT NULL,
-    "limit"      INTEGER      NOT NULL,
+    type       VARCHAR(255)                NOT NULL,
+    "limit"    INTEGER                     NOT NULL,
     CONSTRAINT pk_time_slots PRIMARY KEY (id)
 );
 
